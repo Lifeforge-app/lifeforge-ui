16bd8c98ca7d366d577c078ac7a2f9fca2a9f4e7
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-12 07:32:05 +0800

    25w11

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export * from './components/buttons';
-export * from './components/inputs';
-export * from './components/modals';
-export * from './components/sidebar';
+export * from "./components/buttons";
+export * from "./components/inputs";
+export * from "./components/modals";
+export * from "./components/sidebar";
 export * from "./components/screens";
-export * from './components/utilities';
+export * from "./components/utilities";
 export * from "./providers/LifeforgeUIProvider";
+
+export type { IFieldProps } from "./interfaces/modal_interfaces";
