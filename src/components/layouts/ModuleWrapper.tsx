3bc6e2b67a9ac080bf6e50f22eb8f532eaa9fe2d
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-12 09:38:37 +0800

    25w11

---
+++ src/components/layouts/ModuleWrapper.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import React from "react";
+
+import { Scrollbar } from "@components/utilities";
+
+function ModuleWrapper({
+  children,
+  className = "",
+}: {
+  children: any;
+  className?: string;
+}): React.ReactElement {
+  return (
+    <Scrollbar
+      className={clsx(
+        "no-overflow-x flex min-h-0 flex-col transition-all",
+        className
+      )}
+    >
+      <div className="flex w-full flex-1 flex-col px-4 pt-8 sm:px-12">
+        {children}
+      </div>
+    </Scrollbar>
+  );
+}
+
+export default ModuleWrapper;
