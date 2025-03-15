cc1f558daf6f76520160f0f5d5750e04f1aa5cd5
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-15 20:49:33 +0800

    25w11

--- src/components/utilities/ConfigColumn.tsx
+++ src/components/utilities/ConfigColumn.tsx
@@ -1,8 +1,8 @@
-import { Icon } from "@iconify/react";
-import clsx from "clsx";
-import _ from "lodash";
-import React, { useEffect, useRef } from "react";
-import { Tooltip } from "react-tooltip";
+import { Icon } from '@iconify/react'
+import clsx from 'clsx'
+import React, { useEffect, useRef } from 'react'
+
+import Tooltip from './Tooltip'
 
 function ConfigColumn({
   title,
@@ -13,44 +13,44 @@ function ConfigColumn({
   tooltip,
   hasDivider = true,
   children,
-  wrapWhen = "md",
+  wrapWhen = 'md',
   noDefaultBreakpoints = false,
-  className,
+  className
 }: {
-  title: string | React.ReactNode;
-  desc: string;
-  icon: string;
-  vertical?: boolean;
-  tooltip?: React.ReactNode;
-  hasDivider?: boolean;
-  children: React.ReactNode;
-  wrapWhen?: "sm" | "md" | "lg" | "xl";
-  noDefaultBreakpoints?: boolean;
-  className?: string;
+  title: string | React.ReactNode
+  desc: string
+  icon: string
+  vertical?: boolean
+  tooltip?: React.ReactNode
+  hasDivider?: boolean
+  children: React.ReactNode
+  wrapWhen?: 'sm' | 'md' | 'lg' | 'xl'
+  noDefaultBreakpoints?: boolean
+  className?: string
 }) {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!ref.current) return;
+    if (!ref.current) return
 
-    ref.current.getBoundingClientRect();
-  }, [ref]);
+    ref.current.getBoundingClientRect()
+  }, [ref])
 
   return (
     <>
       <div
         ref={ref}
         className={clsx(
-          "flex w-full min-w-0 flex-col justify-between gap-8 px-4",
+          'flex w-full min-w-0 flex-col justify-between gap-8 px-4',
           !vertical &&
             !noDefaultBreakpoints &&
             {
-              sm: "sm:flex-row",
-              md: "md:flex-row",
-              lg: "lg:flex-row",
-              xl: "xl:flex-row",
+              sm: 'sm:flex-row',
+              md: 'md:flex-row',
+              lg: 'lg:flex-row',
+              xl: 'xl:flex-row'
             }[wrapWhen],
-          vertical && "flex-col",
+          vertical && 'flex-col',
           className
         )}
       >
@@ -60,16 +60,9 @@ function ConfigColumn({
             <h3 className="flex w-full items-center gap-2 text-xl leading-normal font-medium md:w-auto">
               {title}
               {tooltip !== undefined && (
-                <span
-                  data-tooltip-id={`tooltip-${_.kebabCase(
-                    title?.toString() ?? ""
-                  )}`}
-                >
-                  <Icon
-                    className="text-bg-500 size-5"
-                    icon="tabler:info-circle"
-                  />
-                </span>
+                <Tooltip icon="tabler:info-circle" id={title?.toString() || ''}>
+                  {tooltip}
+                </Tooltip>
               )}
             </h3>
             <p className="text-bg-500">{desc}</p>
@@ -78,24 +71,12 @@ function ConfigColumn({
         <div className="flex w-full min-w-0 shrink-0 items-center gap-4 md:w-auto">
           {children}
         </div>
-        {tooltip !== undefined && (
-          <Tooltip
-            className="bg-bg-50 text-bg-800 shadow-custom dark:bg-bg-900 dark:text-bg-50 z-9999 rounded-md! p-4! text-base!"
-            classNameArrow="size-6!"
-            id={`tooltip-${_.kebabCase(title?.toString() ?? "")}`}
-            opacity={1}
-            place="top-start"
-            positionStrategy="fixed"
-          >
-            {tooltip}
-          </Tooltip>
-        )}
       </div>
       {hasDivider && (
         <div className="border-bg-200 dark:border-bg-800/50 my-6 w-full border-b-[1.5px]" />
       )}
     </>
-  );
+  )
 }
 
-export default ConfigColumn;
+export default ConfigColumn
---
+++ src/components/utilities/Tooltip.tsx
@@ -0,0 +1,32 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import _ from 'lodash'
+import React from 'react'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
+
+export default function Tooltip({
+  id,
+  icon,
+  children
+}: {
+  id: string
+  icon: string
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <span data-tooltip-id={`tooltip-${_.kebabCase(id)}`}>
+        <Icon className="text-bg-500 size-5" icon={icon} />
+      </span>
+      <ReactTooltip
+        className="bg-bg-50 text-bg-800 shadow-custom dark:bg-bg-900 dark:text-bg-50 z-9999 rounded-md! p-4! text-base!"
+        classNameArrow="size-6!"
+        id={`tooltip-${_.kebabCase(id)}`}
+        opacity={1}
+        place="top-start"
+        positionStrategy="fixed"
+      >
+        {children}
+      </ReactTooltip>
+    </>
+  )
+}
