ca3b38c9235ddf521b881d65016ceab0e21d3f18
Melvin Chia <melvinchia623600@gmail.com>
2025-05-06 07:04:07 +0800

    25w19

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -2,6 +2,7 @@ export { default as FormModal } from './features/FormModal'
 export { default as DeleteConfirmationModal } from './features/DeleteConfirmationModal'
 
 export { default as ModalHeader } from './components/ModalHeader'
+export { default as ModalWrapper } from './components/ModalWrapper'
 
 export { default as ModalManager } from './core/ModalManager'
 export { default as useModalsEffect } from './core/useModalsEffect'
