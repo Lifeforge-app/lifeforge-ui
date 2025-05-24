5fe721da584b929ed00a24d91ad17e97182d22bf
Melvin Chia <melvinchia623600@gmail.com>
2025-05-24 15:43:12 +0800

    25w21

--- src/components/inputs/DateInput/index.stories.tsx
+++ src/components/inputs/DateInput/index.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { useState } from 'react'
 
-import DateInput from './DateInput'
+import DateInput from '.'
 
 const meta = {
   component: DateInput
--- src/components/inputs/DateInput/index.tsx
+++ src/components/inputs/DateInput/index.tsx
@@ -4,6 +4,8 @@ import DatePicker from 'react-datepicker'
 import { useTranslation } from 'react-i18next'
 import tinycolor from 'tinycolor2'
 
+import './index.module.css'
+
 import { Button } from '@components/buttons'
 
 import useThemeColors from '@hooks/useThemeColor'
