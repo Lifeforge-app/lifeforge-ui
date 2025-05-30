3fcf6711e1ca869607b84394db1347e204fba7e8
Melvin Chia <melvinchia623600@gmail.com>
2025-05-30 23:02:00 +0800

    25w22

--- src/components/inputs/IconInput/IconPickerModal/components/IconEntry.tsx
+++ src/components/inputs/IconInput/IconPickerModal/components/IconEntry.tsx
@@ -11,17 +11,17 @@ function IconEntry({
   onIconSelected: (icon: string) => void
 }) {
   const handleIconSelected = useCallback(() => {
-    onIconSelected(icon)
+    onIconSelected(`${iconSet}:${icon}`)
   }, [icon])
 
   return (
     <button
-      className="flex h-min w-full cursor-pointer flex-col items-center rounded-lg p-4 transition-all hover:bg-bg-200/70 dark:hover:bg-bg-800"
+      className="hover:bg-bg-200/70 dark:hover:bg-bg-800 flex h-min w-full cursor-pointer flex-col items-center rounded-lg p-4 transition-all"
       type="button"
       onClick={handleIconSelected}
     >
       <Icon height="32" icon={`${iconSet}:${icon}`} width="32" />
-      <p className="mt-4 -mb-0.5 text-center text-xs font-medium tracking-wide break-all">
+      <p className="-mb-0.5 mt-4 break-all text-center text-xs font-medium tracking-wide">
         {icon.replace(/-/g, ' ')}
       </p>
     </button>
