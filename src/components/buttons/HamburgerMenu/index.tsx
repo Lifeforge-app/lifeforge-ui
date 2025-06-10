2f588ec98910a0b86c784560dc60894e75959d4d
Melvin Chia <melvinchia623600@gmail.com>
2025-06-10 08:32:00 +0800

    25w24

--- src/components/buttons/HamburgerMenu/index.tsx
+++ src/components/buttons/HamburgerMenu/index.tsx
@@ -32,6 +32,7 @@ function HamburgerMenu(props: MenuProps) {
 
   const handleClick = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
       e.stopPropagation()
       if (onClick !== undefined) {
         onClick(e)
@@ -64,7 +65,7 @@ function HamburgerMenu(props: MenuProps) {
             transition
             anchor={anchor ?? 'bottom end'}
             className={clsx(
-              'z-9999 min-w-[var(--button-width)] overflow-auto rounded-md border border-bg-200 bg-bg-100 text-base text-bg-500 shadow-lg transition duration-100 ease-out [--anchor-gap:12px] empty:invisible focus:outline-hidden data-closed:scale-95 data-closed:opacity-0 dark:border-bg-700 dark:bg-bg-800',
+              'z-9999 border-bg-200 bg-bg-100 text-bg-500 focus:outline-hidden data-closed:scale-95 data-closed:opacity-0 dark:border-bg-700 dark:bg-bg-800 min-w-[var(--button-width)] overflow-auto rounded-md border text-base shadow-lg transition duration-100 ease-out [--anchor-gap:12px] empty:invisible',
               classNames?.menu
             )}
           >
