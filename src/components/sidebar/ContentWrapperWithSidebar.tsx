ce46cf98ea7e80aee3ab35060843c9bda5ebc38d
Melvin Chia <melvinchia623600@gmail.com>
2025-07-06 05:35:34 +0800

    25w28

--- src/components/sidebar/ContentWrapperWithSidebar.tsx
+++ src/components/sidebar/ContentWrapperWithSidebar.tsx
@@ -4,7 +4,7 @@ function ContentWrapperWithSidebar({
   children: React.ReactNode
 }) {
   return (
-    <div className="relative z-[9990] flex h-full flex-1 flex-col lg:ml-6">
+    <div className="relative z-[9990] flex h-full flex-1 flex-col">
       {children}
     </div>
   )
--- src/components/sidebar/LayoutWithSidebar.tsx
+++ src/components/sidebar/LayoutWithSidebar.tsx
@@ -1,5 +1,5 @@
 function LayoutWithSidebar({ children }: { children: React.ReactNode }) {
-  return <div className="flex min-h-0 w-full flex-1">{children}</div>
+  return <div className="flex min-h-0 w-full flex-1 lg:gap-6">{children}</div>
 }
 
 export default LayoutWithSidebar
